1734ae84239be66ed8b51680f2b3250e22c43fa3
Rajat Jain <rajatjain.jain29@gmail.com>
2022-01-28 13:20:12 +0530

    Time: 52 ms (99.36%), Space: 31.6 MB (95.47%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
@@ -1,32 +1,36 @@
-class WordDictionary
-{
-     unordered_map<int, vector<string>> words;
-
-     bool isEqual(string word1, string word2)
-     {
-          for (int i = 0; i < word1.size(); i++)
-          {
-               if (word2[i] == '.')
-                    continue;
-               if (word1[i] != word2[i])
-                    return false;
-          }
-          return true;
-     }
-
+class WordDictionary {
 public:
-     WordDictionary() {}
-
-     void addWord(string word)
-     {
-          words[word.size()].push_back(word);
-     }
-
-     bool search(string word)
-     {
-          for (auto &&s : words[word.size()])
-               if (isEqual(s, word))
-                    return true;
-          return false;
-     }
+    bool isEqual(string w1,string w2){
+        for(int i=0;i<w1.length();i++){
+            if(w2[i] =='.')
+                continue;
+            if(w1[i] != w2[i])
+                return false;
+        }
+        return true;
+    }
+    unordered_map<int,vector<string>> m;
+    WordDictionary() {
+        
+    }
+    
+    void addWord(string word) {
+       m[word.size()].push_back(word);
+    }
+    
+    bool search(string word) {
+        
+       for(auto it : m[word.size()]){
+           if(isEqual(it,word))
+               return true;
+       }
+        return false;
+    }
 };
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
