bed7fffe1db5db8b8cd0a5cfbc4c80dbc196d0e9
Rajat Jain <rajatjain.jain29@gmail.com>
2022-01-29 13:02:35 +0530

    Time: 4 ms (98.98%), Space: 18.3 MB (93.47%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -5,12 +5,14 @@ public:
             return nums.size();
         
         int k = 0;
-        for(int i=1;i<nums.size();i++){
-            if(nums[k] != nums[i]){
-                k++;
-                nums[k] = nums[i];
+        // 1 1 2 2 3 3 3
+        int j = 1;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i] != nums[i+1]){
+                nums[j] = nums[i+1];
+                j++;
             }
         }
-        return k+1;
+        return j;
     }
 };
