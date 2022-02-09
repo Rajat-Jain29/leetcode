aebabe01a460822a847273feac3c685b0a19d4b5
Rajat Jain <rajatjain.jain29@gmail.com>
2022-02-09 10:39:42 +0530

    Time: 16 ms (86.74%), Space: 13.3 MB (37.26%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
-        set<pair<int,int>> s;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if( abs(nums[i] - nums[j]) == k  )
-                   s.insert(  {nums[i],nums[j]} );
+        // sort(nums.begin(),nums.end());
+        // set<pair<int,int>> s;
+        // for(int i=0;i<nums.size();i++){
+        //     for(int j=i+1;j<nums.size();j++){
+        //         if( abs(nums[i] - nums[j]) == k  )
+        //            s.insert(  {nums[i],nums[j]} );
+        //     }
+        // }
+        // return s.size();
+        
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++)
+            m[nums[i]]++;
+        int res = 0;
+        for(auto it : m){
+            if(k==0){
+                if(it.second > 1)
+                    res++;
+            }
+            else{
+                if( m.find( it.first+k  )!=m.end() )
+                    res++;
             }
         }
-        return s.size();
+        return res;
+        
     }
 };
