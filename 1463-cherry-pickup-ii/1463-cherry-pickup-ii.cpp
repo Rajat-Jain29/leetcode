b05e1831f7f6c58b0ce89bed1c619950e3260934
Rajat Jain <rajatjain.jain29@gmail.com>
2022-02-10 05:44:47 +0530

    Time: 180 ms (12.60%), Space: 14.4 MB (37.87%) - LeetHub

---
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int cherryPickup(vector<vector<int>>& grid) {
+        int r = grid.size();
+        int c = grid[0].size();
+      vector<vector<vector<int>>> dp(r,vector<vector<int>>(c,vector<int>(c,0)));
+        for(int j=0;j<c;j++){
+            for(int j1=0;j1<c;j1++){
+                if(j == j1){
+                    dp[r-1][j][j1] = grid[r-1][j];
+                }
+                else
+                    dp[r-1][j][j1] = grid[r-1][j]+grid[r-1][j1];
+            }
+        }
+        // int max = -1e8;
+        for(int i=r-2;i>=0;i--){
+            for(int j1=0;j1<c;j1++){
+                for(int j2=0;j2<c;j2++){
+                  int maxx = -1e8;
+                    for(int dj1=-1;dj1<=1;dj1++){
+                        for(int dj2=-1;dj2<=1;dj2++){
+                            int value=0;
+                            if(j1 == j2){
+                                value = grid[i][j1];
+                            }
+                            else{
+                                value = grid[i][j1]+grid[i][j2];
+                            }
+                            if( j1+dj1 >=0 && j1+dj1<c && j2+dj2 >=0 && j2+dj2 <c )
+                                value += dp[i+1][j1+dj1][j2+dj2];
+                            else
+                                value = -1e8;
+                            maxx = max(maxx,value);
+                        }
+                    }
+                    dp[i][j1][j2] = maxx;
+                }
+            }
+        }
+        return dp[0][0][c-1];
+    }
+};
