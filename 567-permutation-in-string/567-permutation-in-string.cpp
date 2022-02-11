82848cfae2b242cfd0814f1273b63513db0edb0e
Rajat Jain <rajatjain.jain29@gmail.com>
2022-02-11 10:24:16 +0530

    Time: 7 ms (81.21%), Space: 7.2 MB (89.63%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    // bool equal( unordered_map<char,int> &m , unordered_map<char,int> &p ){
+    //     for(int i=0;i<26;i++){
+    //         if(m[i] != p[i])
+    //             return false;
+    //     }
+    //     return true;
+    // }
+    bool checkInclusion(string s1, string s2) {
+        if(s1.length() > s2.length())
+            return false;
+       vector<int> m(26,0),p(26,0);
+        for(int i=0;i<s1.length();i++)
+            m[s1[i] - 'a']++;
+        int i;
+        for(int i=0;i<s1.length();i++)
+            p[s2[i] - 'a']++;
+        
+        for(int i = s1.length();i<s2.length();i++){
+            
+            if( m==p )
+                return true;
+            p[s2[i] -'a' ]++;
+            p[ s2[i-s1.length()] -'a' ]--;
+            
+        }
+        if( m==p )
+            return true;
+        return false;
+        
+    }
+ 
+};
