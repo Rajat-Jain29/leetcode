afc1b2015ca9a675313c315288ed1a9c8f351d6b
Rajat Jain <rajatjain.jain29@gmail.com>
2022-02-15 16:54:46 +0530

    Time: 0 ms, Memory: 6 MB - LeetHub

--- first-bad-version/first-bad-version.cpp
+++ first-bad-version/first-bad-version.cpp
@@ -6,10 +6,10 @@ public:
     int firstBadVersion(int n) {
        int left = 0;
         int right = n;
-        while(left < right){
+        while(left <= right){
             int mid = left + (right-left)/2;
             if( isBadVersion(mid) ){
-                right = mid;
+                right = mid-1;
             }
             else
                 left = mid+1;
