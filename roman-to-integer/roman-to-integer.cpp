b25d9e3091e3c898edfd12be015fcb5581fe5c56
Rajat Jain <rajatjain.jain29@gmail.com>
2022-02-15 13:44:53 +0530

    Time: 23 ms, Memory: 8.4 MB - LeetHub

---
+++ roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char,int> m;
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
+        
+       int res = m[s.length()-1];
+         
+        for(int i=s.length()-1;i>=0;i--){
+            if( m[s[i]] < m[s[i+1]]  )
+                res -= m[s[i]];
+            else
+                res +=  m[s[i]] ;
+        }
+        return res;
+    }
+};
