1cbab6d3043f30aba1c429a152adb2de954b1fec
Rajat Jain <rajatjain.jain29@gmail.com>
2022-02-17 15:44:56 +0530

    Time: 0 ms, Memory: 8.4 MB - LeetHub

--- sort-colors/sort-colors.cpp
+++ sort-colors/sort-colors.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-       sort(nums.begin(),nums.end());
+        int r=0,w=0,b=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] == 0)
+                r++;
+            if(nums[i] == 1)
+                w++;
+            if(nums[i] == 2)
+                b++;
+        }
+        
+        nums.erase(nums.begin(),nums.end());
+        for(int i=0;i<r;i++){
+            nums.push_back(0);
+        }
+         
+        for(int i=0;i<w;i++){
+            nums.push_back(1);
+        }
+        for(int i=0;i<b;i++){
+            nums.push_back(2);
+        }
     }
 };
