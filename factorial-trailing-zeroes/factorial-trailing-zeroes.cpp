677cb5f87cc8903873520655498b80532ea1666b
Rajat Jain <rajatjain.jain29@gmail.com>
2022-02-17 16:41:08 +0530

    Time: 11 ms, Memory: 6.1 MB - LeetHub

---
+++ factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int res = 0;
+        for(int i=1;;i++){
+            
+            if(n / pow(5,i) == 0)
+                break;
+            res += n / pow(5,i);
+            
+        }
+        return res;
+    }
+};
