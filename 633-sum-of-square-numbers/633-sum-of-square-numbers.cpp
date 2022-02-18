0f28278f29b6e87305315271e006b12d0fa32b8f
Rajat Jain <rajatjain.jain29@gmail.com>
2022-02-18 16:36:30 +0530

    Time: 272 ms (6.49%), Space: 71.5 MB (6.08%) - LeetHub

--- 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
@@ -4,11 +4,13 @@ public:
         unordered_map<int,int> m;
         for(int i=0;i<=sqrt(c);i++){
             m[i*i] = i*i;
-        }
-        for(int i=0;i<=sqrt(c);i++){
-             if( m.find(c-m[i*i]) != m.end() )
+            if( m.find(c-m[i*i]) != m.end() )
                  return true;
         }
+//         }
+//         for(int i=0;i<=sqrt(c);i++){
+             
+//         }
         return false;
        
     }
