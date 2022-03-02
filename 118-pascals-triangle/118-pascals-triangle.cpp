cf83dc1d048184a1d1d793bb9c403512450efe9e
Rajat Jain <rajatjain.jain29@gmail.com>
2022-03-02 13:12:25 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (33.83%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-
-      vector<vector<int>> res;
-       vector<int> p;
+        vector<vector<int>> res(numRows);
         for(int i=0;i<numRows;i++){
-            
-            p.insert(p.begin() , 1);
-            for(int j=1;j<p.size()-1;j++){
-                p[j] = p[j] + p[j+1];
-            }
-            res.push_back(p);
+            res[i].resize(i+1);
+            res[i][0] = 1;
+            res[i][i] = 1;
+            for(int j=1;j<i;j++)
+                res[i][j] = res[i-1][j-1]+res[i-1][j];
         }
         return res;
     }
