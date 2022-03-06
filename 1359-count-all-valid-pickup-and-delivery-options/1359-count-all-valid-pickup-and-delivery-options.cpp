203721700cc0865f87944ae4227ef4a5018e2f31
Rajat Jain <rajatjain.jain29@gmail.com>
2022-03-06 07:59:11 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (82.70%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -3,33 +3,17 @@ public:
      int MOD = 1e9 + 7;
     
     int countOrders(int n) {
-        vector<vector<long>> dp (n + 1, vector<long>(n + 1, 0));
-
-        for (int unpicked = 0; unpicked <= n; unpicked++) {
-            for (int undelivered = unpicked; undelivered <= n; undelivered++) {
-                // If all orders are picked and delivered then,
-                // for remaining '0' orders we have only one way.
-                if (unpicked == 0 && undelivered == 0) {
-                    dp[unpicked][undelivered] = 1;
-                    continue;
-                }
-                
-                // There are some unpicked elements left. 
-                // We have choice to pick any one of those orders.
-                if (unpicked > 0) {
-                    dp[unpicked][undelivered] += unpicked * dp[unpicked - 1][undelivered];
-                }
-                dp[unpicked][undelivered] %= MOD;
-                
-                // Number of deliveries done is less than picked orders.
-                // We have choice to deliver any one of (undelivered - unpicked) orders. 
-                if (undelivered > unpicked) {
-                    dp[unpicked][undelivered] += (undelivered - unpicked) * dp[unpicked][undelivered - 1];
-                }
-                dp[unpicked][undelivered] %= MOD;
-            }
+        long base = 1000000007;
+        if (n == 1)
+        {
+            return 1;
         }
-        
-        return dp[n][n];
+        long res = 1;
+        for (int i = 2; i <= n; ++i)
+        {
+            res = ((((long)res*(2*i-1))%base)*i)%base;
+        }
+
+        return res;
     }
 };
