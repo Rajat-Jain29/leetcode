3e42c111a0edad6fa30d243b60900c94a7197fd9
Rajat Jain <rajatjain.jain29@gmail.com>
2022-03-07 09:53:47 +0530

    Time: 6 ms (84.52%), Space: 14.9 MB (45.55%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,35 +11,47 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        vector<int> p;
-        while(l1 != NULL){
-            p.push_back(l1->val);
-            l1=l1->next;
-        }
-        while(l2 != NULL){
-            p.push_back(l2->val);
-            l2=l2->next;
-        }
-        sort(p.begin(),p.end());
-        ListNode *head , *temp = NULL ;
-        for(int i=0;i<p.size();i++){
-            ListNode *newNode = new ListNode();
-            newNode->val = p[i];
-            newNode->next = NULL;
-            if(head == NULL){
-                head = newNode;
-                continue;
-            }
+//         vector<int> p;
+//         while(l1 != NULL){
+//             p.push_back(l1->val);
+//             l1=l1->next;
+//         }
+//         while(l2 != NULL){
+//             p.push_back(l2->val);
+//             l2=l2->next;
+//         }
+//         sort(p.begin(),p.end());
+//         ListNode *head , *temp = NULL ;
+//         for(int i=0;i<p.size();i++){
+//             ListNode *newNode = new ListNode();
+//             newNode->val = p[i];
+//             newNode->next = NULL;
+//             if(head == NULL){
+//                 head = newNode;
+//                 continue;
+//             }
             
-           temp = head;
-            while(temp->next != NULL)
-                temp=temp->next;
+//            temp = head;
+//             while(temp->next != NULL)
+//                 temp=temp->next;
             
-            temp->next = newNode;
+//             temp->next = newNode;
                 
-        }
-        return head;
+//         }
+//         return head;
         
+        if(l1 == NULL)
+            return l2;
+        if(l2 == NULL)
+            return l1;
+        if(l1->val < l2->val){
+           l1->next = mergeTwoLists(l1->next,l2);
+            return l1;
+        }
+        else{
+           l2->next =  mergeTwoLists(l1,l2->next);
+            return l2;
+        }
         
     }
 };
