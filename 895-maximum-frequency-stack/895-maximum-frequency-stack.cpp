97c9915e028707c8e580bc70aba8750545745d3f
Rajat Jain <rajatjain.jain29@gmail.com>
2022-03-19 13:33:15 +0530

    Time: 172 ms (92.40%), Space: 86.4 MB (68.00%) - LeetHub

--- 895-maximum-frequency-stack/895-maximum-frequency-stack.cpp
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.cpp
@@ -1,22 +1,34 @@
 class FreqStack {
 public:
     
+    // unordered_map<int,int> m;
+    // priority_queue<pair<int,pair<int,int>>> p;
+    // int freq = 0;
     unordered_map<int,int> m;
-    priority_queue<pair<int,pair<int,int>>> p;
+    unordered_map<int,stack<int>> s;
     int freq = 0;
     FreqStack() {
         
     }
     
     void push(int val) {
-        p.push( {m[val]++ , { freq++ ,val} } );
+        m[val]++;
+        s[m[val]].push(val);
+        freq = max(freq,m[val]);
+        // p.push( {m[val]++ , { freq++ ,val} } );
     }
     
     int pop() {
-        auto val = p.top();
-        p.pop();
-        int x = val.second.second;
+        int x = s[freq].top();
+        s[freq].pop();
+        if(s[freq].size() == 0)
+             freq--;
         m[x]--;
+       
+        // auto val = p.top();
+        // p.pop();
+        // int x = val.second.second;
+        // m[x]--;
         return x;
     }
 };
