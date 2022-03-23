7912ab09642f2eab7476801a4e48c8837843f6db
Rajat Jain <rajatjain.jain29@gmail.com>
2022-03-23 13:33:13 +0530

    Time: 8 ms (83.53%), Space: 17.4 MB (25.41%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -11,60 +11,29 @@
  */
 class Solution {
 public:
-    
     int widthOfBinaryTree(TreeNode* root) {
-       if(root == NULL) // if root is null 
-            return 0;   // simply return zero
-        
-        int ans = 0; // variable to store answer
-        
-        // queue for level order traveral
-        queue<pair<TreeNode*, int>> q; // pair contain { node, index}
-        
-        q.push({root, 1}); // intially push root node 
-        
-        // Implementing BFS
-        while(q.empty() == false) // until queue is not empty
-        {
-            int size = q.size(); // take size of the queue
-            
-            // tells us minimum index at particular level
-            int minAtLevel = q.front().second; 
-            
-            // declaring minimum and maximum variable used for finding width
-            int mn, mx;
-            
-            // traverse from the queue
-            for(int i= 0; i < size; i++)
-            {
-                // changes index of level by decreasing minimum index
-                int curr_index = q.front().second - minAtLevel + 1; //+ 1 because we are using 1 based indexing,
-                
-                // take out current node
-                TreeNode* node = q.front().first;
-                q.pop(); // pop from the queue
-                
-                // remember the formula we discussed
-            //width = (maximum index at level) - (minimum index at level) + 1
-                
-                if(i == 0) 
-                    mn = curr_index; // minimum index at level
-                
-                if(i == size - 1)
-                    mx = curr_index; //maximum index at level
-                
-                // push left and right for further calculation
-				
-                if(node -> left != NULL) //2   * i
-                    q.push({node -> left, 2LL * curr_index});
-                
-                if(node -> right != NULL) //2   * i  + 1
-                    q.push({node -> right, 2LL * curr_index + 1});
-                   
+        queue< pair<TreeNode* , int> > q;
+        q.push({root , 0});
+        int ans = 0;
+        while(!q.empty()){
+            int size = q.size();
+            int minn = q.front().second;
+            int f,l;
+            for(int i=0;i<size;i++){
+                TreeNode *node = q.front().first;
+                int curr_id = q.front().second - minn;
+                if(i==0)
+                    f = curr_id;
+                if(i==size-1)
+                    l = curr_id;
+                q.pop();
+                if(node->left)
+                    q.push( {node->left , 2LL*curr_id + 1} );
+                 if(node->right)
+                    q.push({node->right , 2LL*curr_id + 2});
             }
-            ans = max(ans, mx - mn + 1); // update our answer
+            ans = max(ans , l-f+1);
         }
-        
-        return ans; // finally return our answer
+        return ans;
     }
 };
