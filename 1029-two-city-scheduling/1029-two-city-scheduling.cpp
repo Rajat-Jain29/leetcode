18ca1e345893d71a61f73b58a18e6cdffbf7357e
Rajat Jain <rajatjain.jain29@gmail.com>
2022-03-25 14:50:17 +0530

    Time: 4 ms (78.75%), Space: 8.5 MB (22.06%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        int n=costs.size()/2;
-    vector<int> refund;
-    
-    int res=0;
-    for(auto it:costs){
-        res+=it[0];  // cost of all for city A
+      int n = costs.size()/2;
+        vector<int> res;
+        priority_queue< pair<int,vector<int> > > q;
+        for(int i=0;i<costs.size();i++){
+            q.push( {costs[i][1] - costs[i][0] , costs[i]}  );
+        //    res.push_back( costs[i][1] - costs[i][0] );
+        }
+        int sum = 0;
+        int i = 0;
+        while(q.size() > n){
+            vector<int> p =  q.top().second;
+            sum += p[0];
+            q.pop();
+        }
+        while(q.size() > 0){
+             vector<int> p =  q.top().second;
+            sum += p[1];
+            
+            q.pop();
+        }
         
-        refund.push_back(it[1]-it[0]);  // -ve means more refund
+        return sum;
     }
-     
-    sort(refund.begin(), refund.end());
-    for(auto it:refund)
-        cout<<it<<" ";
-    for(int i=0;i<n;i++)
-        res+=refund[i]; // refund for n person
-    
-    return res;
-}
 };
