eb53feb8c17b33b4b4cfdb5a3a6642f6a5f5bf0a
Rajat Jain <rajatjain.jain29@gmail.com>
2022-04-12 09:15:13 +0530

    Time: 0 ms (100.00%), Space: 7 MB (58.55%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int val(vector<vector<int>> &a , int i,int j){
+    int valid(vector<vector<int>> &a , int i,int j){
         if(i<0 || j<0 || i>=a.size() || j>= a[0].size())
             return 0;
         return a[i][j];
     }
-    int next(vector<vector<int>> &arr , int i,int j){
-        if(arr[i][j] == 0){
-        int x = val(arr, i + 1, j) + val(arr, i + 1, j + 1) + val(arr, i + 1, j - 1) + val(arr, i, j + 1) + val(arr, i - 1, j) + val(arr, i - 1, j + 1) + val(arr, i - 1, j - 1) + val(arr, i, j-1 );
+    int next(vector<vector<int>> &a , int i,int j){
+        if(a[i][j] == 0){
+        int x = valid(a,i,j-1)+valid(a,i,j+1)+valid(a,i+1,j-1)+valid(a,i+1,j)+valid(a,i+1,j+1)+
+            valid(a,i-1,j-1)+valid(a,i-1,j)+valid(a,i-1,j+1);
             if(x==3)
                 return 1;
             else
                 return 0;
         }
         else{
-    int x = val(arr, i + 1, j) + val(arr, i + 1, j + 1) + val(arr, i + 1, j - 1) + val(arr, i, j + 1) + val(arr, i - 1, j) + val(arr, i - 1, j + 1) + val(arr, i - 1, j - 1) + val(arr, i, j-1 );
+    int x = valid(a,i,j-1)+valid(a,i,j+1)+valid(a,i+1,j-1)+valid(a,i+1,j)+valid(a,i+1,j+1)+
+            valid(a,i-1,j-1)+valid(a,i-1,j)+valid(a,i-1,j+1);
             if(x==2 || x==3)
                 return 1;
             else
