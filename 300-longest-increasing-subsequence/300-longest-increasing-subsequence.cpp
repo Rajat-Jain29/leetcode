b1a41a15a5a1821d86d920f72037810a35d2bfde
Rajat Jain <rajatjain.jain29@gmail.com>
2022-04-14 15:16:36 +0530

    Time: 367 ms (37.36%), Space: 10.5 MB (39.99%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -10,16 +10,28 @@ public:
     // }
     int lengthOfLIS(vector<int>& nums) {
         // return f(0,-1,nums);
+        
+        
+        // int n = nums.size();
+        // vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        // for(int i=n-1;i>=0;i--){
+        //     for(int prev=i-1;prev>=-1;prev--){
+        //         int len = 0 + dp[i+1][prev+1];
+        //         if(prev == -1 || nums[i] > nums[prev])
+        //             len = max(len , 1 + dp[i+1][i+1] );
+        //         dp[i][prev+1] = len;
+        //     }
+        // }
+        // return dp[0][-1+1];
+        
         int n = nums.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-        for(int i=n-1;i>=0;i--){
-            for(int prev=i-1;prev>=-1;prev--){
-                int len = 0 + dp[i+1][prev+1];
-                if(prev == -1 || nums[i] > nums[prev])
-                    len = max(len , 1 + dp[i+1][i+1] );
-                dp[i][prev+1] = len;
+        vector<int> dp(nums.size() , 1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j] < nums[i])
+                    dp[i] = max(dp[i] , 1 + dp[j]);
             }
         }
-        return dp[0][-1+1];
+        return *max_element(dp.begin(),dp.end());
     }
 };
