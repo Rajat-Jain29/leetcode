c316b1f581fdb5b1d6e7c28548c902914a0c9eee
Rajat Jain <rajatjain.jain29@gmail.com>
2022-04-24 23:20:03 +0530

    Time: 4 ms (50.20%), Space: 5.8 MB (92.09%) - LeetHub

---
+++ 600-non-negative-integers-without-consecutive-ones/600-non-negative-integers-without-consecutive-ones.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findIntegers(int n) {
+        int fib[31];
+        fib[0] = 1;
+        fib[1] = 2;
+        for(int i=2;i<30;i++)
+            fib[i] = fib[i-1]+fib[i-2];
+        int ans= 0 ,cnt=30 , prev=0;
+        while(cnt >= 0){
+            if(n & (1<<cnt)  ){
+                ans+=fib[cnt];
+                if(prev == 1)
+                    return ans;
+                prev=1;
+            }
+            else
+                prev=0;
+                
+            cnt--;
+        }
+        return ans+1;
+    }
+};
