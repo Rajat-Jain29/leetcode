2fd9eb2030d04fd4a59c03d9d7a6066edb7e8058
Rajat Jain <rajatjain.jain29@gmail.com>
2022-05-24 08:08:12 +0530

    Time: 5 ms (56.62%), Space: 7.3 MB (67.93%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> st;
+        int  maxx =0;
+        st.push(-1);
+        for(int i=0;i<s.length();i++){
+            if(s[i] == '(')
+                st.push(i);
+           else{
+               st.pop();
+               if(st.empty())
+                   st.push(i);
+               else
+                   maxx = max(maxx,i-st.top());
+           }
+        }
+        return maxx;
+    }
+};
