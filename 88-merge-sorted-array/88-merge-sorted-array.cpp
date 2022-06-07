2b9de2024ed593dd975bcb1484e9f0f820b9c46d
Rajat Jain <rajatjain.jain29@gmail.com>
2022-06-07 07:24:06 +0530

    Time: 6 ms (29.53%), Space: 9.3 MB (30.68%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -2,12 +2,37 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         vector<int> p,q;
-        for(int i=0;i<m;i++)
+        // for(int i=0;i<m;i++)
+        //     p.push_back(nums1[i]);
+        //  for(int i=0;i<n;i++)
+        //     p.push_back(nums2[i]);
+        // sort(p.begin(),p.end());
+        int i=0,j=0;
+        while(i<m && j<n ){
+            if( nums1[i] < nums2[j] ){
+                p.push_back(nums1[i]);
+                i++;
+            }
+            else if( nums1[i] == nums2[j] ){
+                p.push_back(nums1[i]);
+                p.push_back(nums2[j]);
+                i++;
+                j++;
+            }
+             else if( nums1[i] > nums2[j] ){
+                p.push_back(nums2[j]);
+                j++;
+            }
+            
+        }     
+        while(i<m){
             p.push_back(nums1[i]);
-         for(int i=0;i<n;i++)
-            p.push_back(nums2[i]);
-        sort(p.begin(),p.end());
-        
+            i++;
+        }
+        while(j<n){
+            p.push_back(nums2[j]);
+            j++;
+        }
         nums1.clear();
         for(int i=0;i<m+n;i++)
             nums1.push_back(p[i]);
