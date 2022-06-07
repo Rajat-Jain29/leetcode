6b6ffab2e32ee5f3fab034ab4d8581ab3fa4874c
Rajat Jain <rajatjain.jain29@gmail.com>
2022-06-07 07:15:53 +0530

    Time: 3 ms (72.68%), Space: 9.3 MB (30.68%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -4,14 +4,13 @@ public:
         vector<int> p,q;
         for(int i=0;i<m;i++)
             p.push_back(nums1[i]);
-        for(int j=0;j<n;j++){
-            q.push_back(nums2[j]);
-        }
+         for(int i=0;i<n;i++)
+            p.push_back(nums2[i]);
+        sort(p.begin(),p.end());
+        
         nums1.clear();
-        for(int i=0;i<m;i++)
+        for(int i=0;i<m+n;i++)
             nums1.push_back(p[i]);
-        for(int j=0;j<n;j++)
-            nums1.push_back(q[j]);
-        sort(nums1.begin(),nums1.end());
+        // nums1.insert(p.begin(),p.end());
     }
 };
