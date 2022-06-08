9a3f583fe7153aa3c76d69b5c17e3633ebd07d81
Rajat Jain <rajatjain.jain29@gmail.com>
2022-06-08 20:30:39 +0530

    Added solution - LeetHub

--- Subset Sums - GFG/subset-sums.cpp
+++ Subset Sums - GFG/subset-sums.cpp
@@ -7,24 +7,22 @@ class Solution
 {
 public:
     vector<int> res;
-    void solve(int i,vector<int> &arr, int N,int sum, vector<int> &p){
+    void solve(int i,vector<int> &arr, int N, vector<int> &p){
         if(i==N){
            int k = accumulate(p.begin(),p.end(),0);
            res.push_back(k);
            return;
         }
         p.push_back(arr[i]);
-        sum += arr[i];
-        solve(i+1,arr,N,sum,p);
+        solve(i+1,arr,N,p);
         p.pop_back();
-        sum -= arr[i];
-        solve(i+1,arr,N,sum,p);
+        solve(i+1,arr,N,p);
     }
     vector<int> subsetSums(vector<int> arr, int N)
     {
         // Write Your Code here
         vector<int> p;
-        solve(0,arr,N,0,p);
+        solve(0,arr,N,p);
         return res;
     }
 };
