f0b9d9211be0e52d2e6c58a8df256ef9a46602aa
Rajat Jain <rajatjain.jain29@gmail.com>
2022-06-11 12:52:54 +0530

    Time: 764 ms (15.87%), Space: 153.5 MB (27.26%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-       int total=0,n=nums.size();
-        for(int num : nums)total+=num;
-        int need=total-x,cur=0,longest=0;
-        if(need==0)return n;
-        int l=0,r=0;
-        while(l<=r){
-            if(cur<need){
-                if(r<n)cur+=nums[r++];
-                else break;
-            }else if(cur>need){
-                cur-=nums[l++];
-            }else{
-                longest=max(longest,r-l);
-                cur-=nums[l++];
+        int n = nums.size();
+        int target = 0;
+        for(auto it : nums)
+            target += it;
+        target = target - x;
+        if(target == 0)
+            return n;
+        int sum = 0;
+        int res = -1;
+        
+        unordered_map<int,int> m;
+        m[0] = -1;
+        for(int i=0;i<nums.size();i++){
+            sum += nums[i];
+            if( m.find( sum - target  ) != m.end() ){
+                res = max(res,  i-m[sum-target]);
             }
+            m[sum] = i;
         }
-        if(cur==need)longest=max(longest,r-l);
-        if(longest==0)return -1;
-        return n-longest;
+        return res == -1 ? -1 : n - res;
     }
 };
