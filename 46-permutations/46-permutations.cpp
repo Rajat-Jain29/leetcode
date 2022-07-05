188dd8b6d07221fca75ebc5f293b5ce3a42c2567
Rajat Jain <rajatjain.jain29@gmail.com>
2022-07-05 10:31:10 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (37.60%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> res;
-   void helper(vector<int> &nums,int i){
-       if(i == nums.size()){
-           res.push_back(nums);
-           return ;
+   void helper(vector<int> &nums,vector<int> &p,vector<int> &freq){
+        if(p.size() == nums.size()){
+            res.push_back(p);
+            return ;
+        }
+       for(int i=0;i<nums.size();i++){
+           if( freq[i] == 0){
+               p.push_back(nums[i]);
+               freq[i] = 1;
+               helper(nums,p,freq);
+               freq[i] = 0;
+               p.pop_back();
+           }
        }
-       for(int j=i;j<nums.size();j++){
-           swap(nums[i] , nums[j]);
-           helper(nums,i+1);
-           swap(nums[i] , nums[j]);
-
-       }
-
-        // if(nums.size() == 0)
-        //     return {{}};
-        // vector<vector<int>> res;
-        // for(int i=0;i<nums.size();i++){
-        //     vector<int> temp;
-        //     for(int j=0;j<nums.size();j++){
-        //         if(i!=j){
-        //             temp.push_back(nums[j]);
-        //         }
-        //     }
-        //    vector<vector<int>> v = helper(temp);
-        //     for(auto it: v){
-        //         it.push_back(nums[i]);
-        //         res.push_back(it);
-        //     }
-        // }
-        // return res;
     }
     vector<vector<int>> permute(vector<int>& nums) {
-         helper(nums,0);
+        int n = nums.size();
+        vector<int> p;
+        vector<int> freq(n,0);
+        helper(nums,p,freq);
         return res;
     }
 };
