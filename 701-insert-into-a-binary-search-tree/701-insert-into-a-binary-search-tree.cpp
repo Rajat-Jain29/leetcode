fd4bd42308daa54fe1c8b09fa9d7f06810b791c8
Rajat Jain <rajatjain.jain29@gmail.com>
2022-08-17 11:21:31 +0530

    Time: 124 ms (68.01%), Space: 57 MB (48.78%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
@@ -11,28 +11,40 @@
  */
 class Solution {
 public:
-    TreeNode* insertIntoBST(TreeNode* root, int val) {
-        if(root == NULL)
+    TreeNode* solve(TreeNode* root, int val){
+        if(root == NULL){
             return new TreeNode(val);
-        TreeNode *curr = root;
-        while(curr!=NULL){
-            if(curr->val < val){
-                 if(curr->right != NULL)
-                     curr=curr->right;
-                else{
-                    curr->right = new TreeNode(val);
-                    break;
-                }
-            }
-            else{
-                  if(curr->left != NULL)
-                     curr=curr->left;
-                else{
-                    curr->left = new TreeNode(val);
-                    break;
-                }
-            }
         }
+        if(root->val > val)
+            root->left = solve(root->left,val);
+        else
+            root->right = solve(root->right,val);
         return root;
+    }
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        TreeNode* v = solve(root,val);
+        return v;
+        // if(root == NULL)
+        //     return new TreeNode(val);
+        // TreeNode *curr = root;
+        // while(curr!=NULL){
+        //     if(curr->val < val){
+        //          if(curr->right != NULL)
+        //              curr=curr->right;
+        //         else{
+        //             curr->right = new TreeNode(val);
+        //             break;
+        //         }
+        //     }
+        //     else{
+        //           if(curr->left != NULL)
+        //              curr=curr->left;
+        //         else{
+        //             curr->left = new TreeNode(val);
+        //             break;
+        //         }
+        //     }
+        // }
+        // return root;
     }
 };
