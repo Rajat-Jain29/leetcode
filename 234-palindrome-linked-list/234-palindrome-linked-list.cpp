22ab320f8ac9e2efc57b43915c690b4e2b493ab6
Rajat Jain <rajatjain.jain29@gmail.com>
2022-08-23 12:29:41 +0530

    Time: 447 ms (26.95%), Space: 118 MB (59.47%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -10,33 +10,32 @@
  */
 class Solution {
 public:
-    ListNode* rev(ListNode *temp){
-        ListNode *res = NULL;
+    ListNode *rev(ListNode *temp){
+        ListNode *dummy = NULL;
         while(temp!=NULL){
             ListNode *nx = temp->next;
-            temp->next = res;
-            res = temp; 
-            temp=nx;
+            temp->next = dummy;
+            dummy = temp;
+            temp = nx;
         }
-        return res;
+        return dummy;
     }
     bool isPalindrome(ListNode* head) {
-        if(head==NULL || head->next==NULL )
-            return true;
         ListNode *slow = head;
         ListNode *fast = head;
-        while(fast->next!= NULL && fast->next->next!=NULL){
+        while( fast->next!=NULL && fast->next->next!=NULL){
             slow=slow->next;
             fast=fast->next->next;
         }
         slow->next = rev(slow->next);
         slow = slow->next;
-        while(slow != NULL){
-            if(head->val != slow->val)
+        while(slow != NULL && head!=NULL ){
+            if(slow->val != head->val)
                 return false;
-            head=head->next;
             slow=slow->next;
+            head=head->next;
         }
         return true;
+        
     }
 };
