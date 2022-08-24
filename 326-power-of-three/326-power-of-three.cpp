e244d1250490ab7be99ac21b61d864d4f68510ed
Rajat Jain <rajatjain.jain29@gmail.com>
2022-08-24 09:25:53 +0530

    Time: 51 ms (14.73%), Space: 6.1 MB (12.98%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        for(int i=0;;i++){
+        for(int i=0;i<=log2(n);i++){
             if( pow(3,i) == n )
                 return true;
-            if(pow(3,i) > n)
-                return false;
         }
         return false;
     }
