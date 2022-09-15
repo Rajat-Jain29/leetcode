e1b9c8ee1fbdfe5973ac439d6a6349d551531d04
Rajat Jain <rajatjain.jain29@gmail.com>
2022-09-15 11:28:04 +0530

    Time: 1082 ms (10.57%), Space: 196.2 MB (11.88%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    vector<int> findOriginalArray(vector<int>& A) {
-         if (A.size() % 2) return {};
-        multiset<int> s(begin(A), end(A));
-        vector<int> ans;
-        int N = A.size();
-        for (int i = 0; i < N; i += 2) {
-            int n = *s.begin();
-            ans.push_back(n);
+    vector<int> findOriginalArray(vector<int>& changed) {
+        multiset<int> s( changed.begin() , changed.end() );
+        int n = changed.size();
+        if( n % 2 == 1)
+            return {};
+        vector<int> res;
+        for(int i=0;i<n;i+=2){
+            int x = *s.begin();
+            res.push_back(x);
             s.erase(s.begin());
-            if (s.find(2 * n) == s.end()) return {}; // Don't use `s.count(2 * n) == 0` here since it's an `O(N)` operation for `multiset`.
-            s.erase(s.find(2 * n));
+            if( s.find( 2*x ) == s.end() )
+                return {};
+            s.erase( s.find(2*x)  );
         }
-        return ans;
-        
-        
-       
+        return res;
     }
 };
