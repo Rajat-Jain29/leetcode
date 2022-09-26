d9e72082e233ac11fefd21fdf86b6e178c95dbff
Rajat Jain <rajatjain.jain29@gmail.com>
2022-09-26 09:44:38 +0530

    Time: 3 ms (90.26%), Space: 11.1 MB (75.03%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,49 +1,37 @@
 class Solution {
 public:
-    int pivot(vector<int>& nums){
-        int left = 0;
-        int right = nums.size()-1;
-        
-        while(left<right){
-            int mid = left + (right-left)/2;
-            
-            if(nums[mid] >= nums[0] ){
-                left = mid+1;
+    int solve(vector<int> &nums){
+        int l = 0;
+        int r = nums.size()-1;
+        while(l < r){
+            int mid = l + (r-l)/2;
+            if(nums[0] <= nums[mid] ){
+                l = mid+1;
             }
-            else{
-                right = mid;
-            }
-            
-        }
-        return left;
-    }
-    
-    int binary(vector<int>& nums, int l,int r,int target){
-        
-        int left = l;
-        int right = r;
-        while(left <= right){
-            int mid = left + (right-left)/2;
-            if(nums[mid] == target)
-                return mid;
-            else if(nums[mid] > target)
-                right = mid-1;
             else
-                left = mid+1;
+                r = mid;
+        }
+        return l;
+    }
+    int binary(vector<int> &nums,int l, int r, int tar){
+        while(l <= r){
+            int mid = l + (r-l)/2;
+            if(nums[mid] == tar){
+                return mid;
+            }
+            else if( nums[mid] > tar )
+                r = mid-1;
+            else
+                l = mid+1;
         }
         return -1;
-        
     }
     int search(vector<int>& nums, int target) {
-        int x = pivot(nums);
+        int pivot = solve(nums);
         int n = nums.size();
-      
-        
-        if(target >=nums[x] && target<= nums[n-1] ){
-            return binary(nums,x,n-1,target);
-        }
-        else{
-            return binary(nums,0,x-1,target);
-        }
+        if( target >= nums[pivot] && target <= nums[nums.size()-1] )
+            return binary(nums,pivot,n-1,target);
+        else
+            return binary(nums,0,pivot-1,target);
     }
 };
